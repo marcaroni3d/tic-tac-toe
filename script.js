6b6853278dcfe2338b3b8b264daa5e46a760881c
marcaroni3d <marc.eddington@gmail.com>
2023-08-22 21:55:16 -0400

    version 1.0: working UI. Does not yet check for winner

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+const Player = (sign) => {
+    this.sign = sign
+
+    const getSign = () => {
+        return sign
+    }
+
+    return { getSign }
+};
+
 const Gameboard = (() => {
-    let board = ['1','2','3','4','5','6','7','8','9'];
+    let board = ['','','','','','','','',''];
 
     const setField = (index, sign) => {
         if (index > board.length) return
@@ -14,17 +24,28 @@ const Gameboard = (() => {
             board[i] = ''
         }
     }
+
     return { setField, getField, reset }
 })();
 
-const Player = (name, sign) => {
-    const getName = () => name;
-    const getSign = () => sign;
-    return { getName, getSign }
-}
-
 const displayController = (() => {
     const gameCells = document.querySelectorAll('.game-cell')
+    const mainText = document.getElementById('main-text')
+    const restartButton = document.getElementById('restart-button')
+
+    gameCells.forEach(cell => 
+        cell.addEventListener('click', (e) => {
+            if (gameController.getIsOver() || e.target.textContent !== '') return
+            gameController.playRound(e.target.dataset.index)
+            updateGameboard()
+        }))
+
+    restartButton.onclick = (e) => {
+        Gameboard.reset()
+        gameController.reset()
+        updateGameboard()
+        setMainText("Player X's turn")
+    }
 
     const updateGameboard = () => {
         for (let i = 0; i < gameCells.length; i++) {
@@ -32,8 +53,67 @@ const displayController = (() => {
         }
     }
 
-    updateGameboard()
-})
+    const setResultMessage = (winner) => {
+        setMainText(`Player ${winner} has won!`)
+    }
+
+    const setMainText = (message) => {
+        mainText.textContent = message
+    }
+
+    return { setResultMessage, setMainText }
+})();
+
+const gameController = (() => {
+    const playerX = Player('X')
+    const playerO = Player('O')
+    let round = 1
+    let isOver = false
+
+    const playRound = (index) => {
+        Gameboard.setField(index, getCurrentPlayerSign())
+        if (checkWinner(index)) {
+            displayController.setResultMessage(getCurrentPlayerSign())
+            isOver = true
+            return
+        }
+        if (round === 9) {
+            displayController.setMainText('Draw game')
+            isOver = true
+            return
+        }
+        round++
+        displayController.setMainText(`Player ${getCurrentPlayerSign()}'s turn`)
+    }
+
+    const getCurrentPlayerSign = () => {
+        return round % 2 === 1 ? playerX.getSign() : playerO.getSign()
+    }
+
+    const checkWinner = (index) => {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+    }
+
+    const getIsOver = () => {
+        return isOver
+    }
+
+    const reset = () => {
+        round = 1
+        isOver = false
+    }
+
+    return { playRound, getIsOver, reset }
+})();
 
 // INIT
-displayController()
+displayController.setMainText("Player X's turn")
