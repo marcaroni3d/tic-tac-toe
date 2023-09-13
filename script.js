1739fffe2b71ea1c0940e1c2a67c81c458475992
marcaroni3d <marc.eddington@gmail.com>
2023-09-13 13:55:58 -0400

    version 1.2: misc fixes

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-// To Do:
-// Selection onclick => change icon, set game mode, keep button active
-    // function to get random computer choice
-    // create game modes
-// start game onclick => hide modal
-// display roundCount
+// instead of main game functionality happening on click,
+// create master game manager function
+    // let current turn = x
+    // check if Bot
+        // if so getRandomChoice
+    // check if Player
+        // if so clicklistener
+    
+    // loop until winner
+
 
 // MAIN GAME FUNCTIONALITY
-
 const Player = (sign) => {
     this.sign = sign
 
@@ -34,31 +37,35 @@ const Gameboard = (() => {
         }
     }
 
-    return { setField, getField, reset }
+    return { board, setField, getField, reset }
 })();
 
+
+// DISPLAY
 const displayController = (() => {
-    const gameSetupModal = document.getElementById('game-setup-modal')
-    const startGameButton = document.getElementById('start-game-button')
     const mainText = document.getElementById('main-text')
     const gameCells = document.querySelectorAll('.game-cell')
     const xScoreDisplay = document.getElementById('x-score-display')
     const oScoreDisplay = document.getElementById('o-score-display')
     const roundDisplay = document.getElementById('round-display')
-    const newMatchButton = document.getElementById('new-match-button')
+    const newRoundButton = document.getElementById('new-round-button')
     const newGameButton = document.getElementById('new-game-button')
 
-    startGameButton.addEventListener('click', () => {
-        gameSetupModal.style.visibility = 'hidden'
-    })
+    gameCells.forEach(cell => 
+        cell.addEventListener('click', (e) => {
+            if (gameController.getIsOver() || e.target.textContent !== '') return
+            gameController.playRound(parseInt(e.target.dataset.index))
+            updateGameboard()
+        }))
 
-    newMatchButton.onclick = () => {
+    newRoundButton.onclick = () => {
         statsController.addRoundCount()
         resetGame()
     }
 
     newGameButton.onclick = () => {
-        gameSetupModal.style.visibility = 'visible'
+        gameSetupController.removeActiveSelections()
+        gameSetupController.showModal()
         statsController.clear()
         resetGame()
     }
@@ -69,13 +76,6 @@ const displayController = (() => {
         updateGameboard()
         setMainText("Player X's turn")
     }
-
-    gameCells.forEach(cell => 
-        cell.addEventListener('click', (e) => {
-            if (gameController.getIsOver() || e.target.textContent !== '') return
-            gameController.playRound(parseInt(e.target.dataset.index))
-            updateGameboard()
-        }))
 
     const updateGameboard = () => {
         for (let i = 0; i < gameCells.length; i++) {
@@ -100,48 +100,109 @@ const displayController = (() => {
     return { setResultMessage, setMainText, updateStats }
 })();
 
-const statsController = (() => {
-    let xScore = 0
-    let oScore = 0
-    let roundCount = 1
 
-    const addXScore = () => {
-        xScore++
-        updateStats()
+// GAME SETUP MODAL
+const gameSetupController = (() => {
+    let xUserType = 'Player'
+    let oUserType = 'Bot'
+
+    const getXUserType = () => {
+        return xUserType
+    }
+    const getOUserType = () => {
+        return oUserType
     }
 
-    const addOScore = () => {
-        oScore++
-        updateStats()
+    const xButtons = document.querySelectorAll('.x-button')
+    const oButtons = document.querySelectorAll('.o-button')
+    const xPic = document.getElementById('x-pic')
+    const oPic = document.getElementById('o-pic')
+    const selectionButtons = document.querySelectorAll('.selection-button')
+    const startGameButton = document.getElementById('start-game-button')
+    const gameSetupModal = document.getElementById('game-setup-modal')
+
+    const showModal = () => {
+        gameSetupModal.style.visibility = 'visible'
     }
 
-    const addRoundCount = () => {
-        roundCount++
-        updateStats()
+    const hideModal = () => {
+        gameSetupModal.style.visibility = 'hidden'
+    }
+    
+    selectionButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const choice = e.target.innerText
+            const user = e.target.parentNode.parentNode.id
+
+            if (user === 'x-container') {
+                xButtons.forEach(btn => btn.classList.remove('selection-button-active'))
+                xUserType = choice
+                if (xUserType === 'Player') {
+                    xPic.src = 'images/account.svg'
+                }
+                if (xUserType === 'Bot') {
+                    xPic.src = 'images/robot.svg'
+                }
+            }
+            if (user === 'o-container') {
+                oButtons.forEach(btn => btn.classList.remove('selection-button-active'))
+                oUserType = choice
+                if (oUserType === 'Player') {
+                    oPic.src = 'images/account.svg'
+                }
+                if (oUserType === 'Bot') {
+                    oPic.src = 'images/robot.svg'
+                } 
+            }
+
+            button.classList.add('selection-button-active')
+        })
+    })
+
+    const removeActiveSelections = () => {
+        xButtons.forEach(button => {
+            button.classList.remove('selection-button-active')
+        })
+        oButtons.forEach(button => {
+            button.classList.remove('selection-button-active')
+        })
     }
 
-    const updateStats = () => {
-        displayController.updateStats(xScore, oScore, roundCount)
-    }
+    startGameButton.addEventListener('click', () => {
+        // check that selections are made for both x and o, or default, or give error and return
+        gameController.init()
+        hideModal()
+    })
 
-    const clear = () => {
-        xScore = 0
-        oScore = 0
-        roundCount = 1
-        updateStats()
-    }
-
-    return { addXScore, addOScore, addRoundCount, clear}
+    return { getXUserType, getOUserType, showModal, hideModal, removeActiveSelections }
 })();
 
+
+// GAME CONTROLLER
 const gameController = (() => {
     const playerX = Player('X')
     const playerO = Player('O')
+    let currentTurn = 'X'
+    let playerXType
+    let playerOType
     let round = 1
     let isOver = false
 
+    // game init function to be fired on modal start game button click:
+    // grab x & o player types
+  
+    // trigger getComputerChoice if currentplayer is a bot
+
+    const init = () => {
+        playerXType = gameSetupController.getXUserType()
+        playerOType = gameSetupController.getOUserType()
+    }
+
     const playRound = (index) => {
-        Gameboard.setField(index, getCurrentPlayerSign())
+        if ((currentTurn === 'X' && playerXType === 'Player') || 
+            (currentTurn === 'O' && playerOType === 'Player')) {
+            Gameboard.setField(index, getCurrentPlayerSign())
+        }
         if (checkWinner(index)) {
             displayController.setResultMessage(getCurrentPlayerSign())
             if (getCurrentPlayerSign() === 'X') {
@@ -196,8 +257,55 @@ const gameController = (() => {
         isOver = false
     }
 
-    return { playRound, getIsOver, reset }
+    return { init, playRound, getIsOver, reset }
 })();
+
+// BOTS
+// const bots = (() => {
+//     const possibleMoves = [];
+//     Gameboard.board.filter((el, i) => {
+//         if(el === null) {
+//             possibleMoves.push(i)
+//         }
+//     })
+//     const randNum = Math.floor(Math.random() * 9)
+//     playRound(randNum)
+// })();
+
+const statsController = (() => {
+    let xScore = 0
+    let oScore = 0
+    let roundCount = 1
+
+    const addXScore = () => {
+        xScore++
+        updateStats()
+    }
+
+    const addOScore = () => {
+        oScore++
+        updateStats()
+    }
+
+    const addRoundCount = () => {
+        roundCount++
+        updateStats()
+    }
+
+    const updateStats = () => {
+        displayController.updateStats(xScore, oScore, roundCount)
+    }
+
+    const clear = () => {
+        xScore = 0
+        oScore = 0
+        roundCount = 1
+        updateStats()
+    }
+
+    return { addXScore, addOScore, addRoundCount, clear}
+})();
+
 
 // INIT
 displayController.setMainText("Player X's turn")
